b7d0dc90f397acaee71cb339b06ec76c1f494baa
BigPoppa <142349497+BigPoppaT@users.noreply.github.com>
2023-08-31 20:26:04 -0700

    Update register.js
    I changed the code now it should work on the website and should be able to match the passwords correctly.

--- register.js
+++ register.js
@@ -1,10 +1,16 @@
-function matchPassword() {  
-    var pw1 = document.getElementById("pswd1");  
-    var pw2 = document.getElementById("pswd2");  
-    if(pw1 != pw2)  
-    {   
-      alert("Passwords did not match");  
-    } else {  
-      alert("Password created successfully");  
-    }  
-}  
+
+const passwordMatch = () => {
+  while(true) {
+    let pw1 = document.getElementById("pswd1");
+    let pw2 = document.getElementById("pswd2");
+    if(pw1 === pw2) {
+      alert("Password created successfully");
+      return passwordMatch;
+    } else {
+      alert("Passwords did not match, Try again!")
+    }
+  }
+};
+
+
+passwordMatch();
